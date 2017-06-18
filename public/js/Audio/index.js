eea2912af845e23956ceac661ad4ebdd6591c035
Howard Jian <hcwjian@gmail.com>
2017-06-18 13:27:17 -0400

    MVP built

--- public/js/Audio/index.js
+++ public/js/Audio/index.js
@@ -3,10 +3,10 @@
 // 1. Set the Audio context as determined by browser
 // 2. Create an XMLHttpRequest, as this has a response type of "ArrayBuffer", used to handle async requests such as audio buffers
 // 3. Decode the audio data found in the audio context, handling the callback and errors
-// 4. create a ScriptProcessingNode to handle processing/analyzing of audio via JS
-// 5. create a buffer source node
-
-
+// 4. create a script processing node and audio buffer source as well as an analyzer node
+// 5. set the appropriate size of fourier transform, which will also set the binfrequency size
+// 6. connect Nodes
+// 7. create a scaling factor to be used globally (in the visual file) that scales with the average frequency size, which is obtained from getByteFrequencyData method, called off of the analyzerNode
 
 let context, JSAudioNode, audioSourceNode, analyzerNode;
 // context = window.AudioContext || window.webkitAudioContext;
@@ -51,55 +51,40 @@ request.onload = function() {
     // choosing a fft size of 512 sets a frequencyBinCount of 256 (half of this), which allows us to use Uint8array which takes up to 8 bits of info (256)
     // This allows us to manage, send and respond to smaller bytes of frequency data (as opposed to the float 32)
     analyzerNode.fftSize = 512;
-    // connecting Nodes
-    // JSAudioNode.connect(AudioContext.destination);
     JSAudioNode.buffer = decodedDataBuffer;
+    audioSourceNode.buffer = decodedDataBuffer;
+    audioSourceNode.loop = true;
+
+    // connecting Nodes
+    JSAudioNode.connect(context.destination);
     audioSourceNode.connect(analyzerNode);
     analyzerNode.connect(JSAudioNode);
     audioSourceNode.connect(context.destination) // speakers
 
+    // handles the event that fires off when audio is being processed
     JSAudioNode.onaudioprocess = (event) => {
       let audioFreqArr = new Uint8Array(analyzerNode.frequencyBinCount);
+
+      // this normalizes the frequency data to an 8-byte number (0-255)
+      // as opposed to the Float32 array which normalizes to 1024
       analyzerNode.getByteFrequencyData(audioFreqArr);
 
       // to scale the cube's size, we need to get the average frequency in the audioFreqArr
-      audioFreqArr.forEach(freq => cubeAudioScale += freq);
-      cubeAudioScale / audioFreqArr.length
+      audioFreqArr.forEach(freq => {
+        // console.log(freq);
+        cubeAudioScale += freq
+      });
+
+      cubeAudioScale /= audioFreqArr.length
+      // console.log(cubeAudioScale);
     }
-
+    // start the song once everything is hooked up
     audioSourceNode.start();
-
   })
   .catch(error => {
     // some kind of decoding error
     console.error(error);
   })
-}
+};
 
-
-
-
-
-
-
-
-
-
-//
-// let camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
-//
-// // audio listener that is added to the camera
-// let listener = new THREE.AudioListener();
-// camera.add(listener);
-//
-// // global audio source
-// let sound = new THREE.Audio(listener);
-//
-// let audioLoader = new THREE.AudioLoader();
-//
-// audioLoader.load(url, function (buffer){
-//   sound.setBuffer(buffer);
-//   sound.setLoop(true);
-//   sound.setVolume(0.5);
-//   sound.play();
-// })
+request.send();
--- public/js/Visual/index.js
+++ public/js/Visual/index.js
@@ -3,13 +3,15 @@ let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth/ window.innerHeight, .1, 1000);
 
 camera.position.x = 0;
-camera.position.y = 10;
+camera.position.y = 0;
 camera.position.z = 30;
 
 
 let renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight);
 document.body.appendChild( renderer.domElement );
+
+// LIGHT: without light, the metallic surface (phongmaterial) will not show
 
 let lightA = new THREE.AmbientLight(0x505050);
 scene.add(lightA);
@@ -35,12 +37,15 @@ scene.add(directionalLight);
 
 // CUBES RENDERING
 // cool color from Three.js example: 0x2194ce
-let geometry = new THREE.CubeGeometry( 1.5, 1.5, 1.5 );
+let geometry = new THREE.CubeGeometry( 1, 1, 1 );
+// let geometry = new THREE.SphereGeometry( 1, 32, 32 );
+
 
 
 let cubes = [];
 let cube;
-// Being to make a 3D sphere:
+
+// Begin to make a 3D sphere:
 // For we make vertical slices on the Y array, with each level being 2 meters apart:
 // increment = true,
 // yCoords = 0,
@@ -53,6 +58,13 @@ for(let yCoords = 0; yCoords < 22; yCoords += 2){
   let radius = incrementing ? y : 20 - y;
   let degree = 0;
   let numCubes = incrementing ? 4 * (yCoords / 2) + 1 : 4 * (20 - yCoords) / 2 + 1 ;
+
+  // In order to influence the scaling provided the music and only pulse the outward-facing faces of the cube, I need to determine which quandrants each physical object is in the 3D space
+
+  let verticalHalf, horizontalHalf, zHalf, convert;
+  if(y < 10) verticalHalf = '-'
+  else if(y === 10) verticalHalf = '='
+  else verticalHalf = '+';
 
   // Each slice of the sphere (a circle whose variable points are on the X-axis and Z-axis) should contain 16 points, 1 for each major direction and 3 in each quandrant
 
@@ -67,6 +79,30 @@ for(let yCoords = 0; yCoords < 22; yCoords += 2){
   for(let currentCube = 0; currentCube < numCubes; currentCube++) {
     let x = radius * Math.sin(degree);
     let z = radius * Math.cos(degree);
+
+    if(!x) horizontalHalf = '='
+    else if(x > 0) horizontalHalf = '+'
+    else horizontalHalf = '-';
+
+    if(!z) zHalf = '='
+    else if(z > 0) zHalf = '-'
+    else zHalf = '+';
+
+    // To determine the octant of a sphere
+    // ** If you are a middle, you do NOT pulse in that axis
+    // FORMAT: horizontalHalf, verticalHalf, zHalf
+    // + + + : right, top, toward
+    // + + - : right, top, away
+    // + - + : right, bottom, toward
+    // + - - : right, bottom, away
+    // - + + : left, top, toward
+    // - + - : left, top, away
+    // - - + : left, bottom, toward
+    // - - - : left, bottom, away
+
+
+    // octant = horizontalHalf + verticalHalf + zHalf
+
     let material =
     new THREE.MeshPhongMaterial({
       color: generateRandomColor(),
@@ -78,12 +114,25 @@ for(let yCoords = 0; yCoords < 22; yCoords += 2){
     });
 
     cube = new THREE.Mesh(geometry, material);
+
+    cube.userData = {
+      vertical: verticalHalf,
+      horizontal: horizontalHalf,
+      zDirection: zHalf
+      // octant: octant
+    }
+
     cube.position.set(x, y, z);
     scene.add(cube);
     degree += Math.PI/(numCubes/2);
     cubes.push(cube);
   }
 }
+
+cubes.forEach(cube => {
+  cube.translateY(-10);
+  console.log(cube);
+})
 
 // Functions
 
@@ -101,9 +150,28 @@ function generateRandomColor() {
 
 function render() {
   requestAnimationFrame( render );
+
+  // this will be added to the scaling factor to add a 'randomization' to each cube object within the sphere
+  let i = 0;
   cubes.forEach( cube => {
-    cube.rotation.x += Math.random()/10;
-    cube.rotation.y += Math.random()/10;
+    i += 1;
+    if( i === 10) {
+      i = 0
+    };
+
+
+    cube.scale.x = cubeAudioScale && (cube.userData.horizontal !== '=') ?
+    (cube.userData.horizontal === '+' ? (i/20 + cubeAudioScale / 100) :
+    -i/20 - cubeAudioScale/100) : 1
+
+    cube.scale.y = cubeAudioScale && (cube.userData.vertical !== '=') ?
+    (cube.userData.vertical === '+' ? (i/20 + cubeAudioScale / 100) :
+    -i/20 - cubeAudioScale/100) : 1
+
+    cube.scale.z = cubeAudioScale && (cube.userData.zHalf !== '=') ?
+    (cube.userData.zHalf === '+' ? (i/20 + cubeAudioScale / 100) :
+    -i/20 - cubeAudioScale/100) : 1
+
   })
   renderer.render( scene, camera );
 }
