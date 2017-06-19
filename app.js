2a5a4ab6eb821880256be7876a4f5aec7774c96a
Howard Jian <hcwjian@gmail.com>
2017-06-19 02:31:43 -0400

    another heroku fix

--- app.js
+++ app.js
@@ -3,4 +3,4 @@ const app = express();
 
 app.use('/', express.static(__dirname + '/public'));
 
-app.listen(8080, () => console.log('server is up and running'));
+app.listen(process.env.PORT || 3000, () => console.log('server is up and running'));
